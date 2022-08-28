a4b7237f87c1558f3d4d58f84f237ff9ed23fe46
Long56642002 <longhoangdinh212@gmail.com>
2022-08-28 11:16:30 +0700

    fix json response

--- CheckApp/views.py
+++ CheckApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
+from django.http import JsonResponse
 from CheckApp.final import find_plagiarism
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -24,6 +24,9 @@ def imageCheckerApi(request):
         # image_serializer=ImageCheckerSerializer(data=checker,many=True)
         # if image_serializer.is_valid():
         #     return JsonResponse(True,safe=False)
-        return JsonResponse(checker,safe=False)
+        return JsonResponse({
+            'success':True,
+            'data':checker
+        },safe=False)
     # elif request.method=='GET':
     #     return JsonResponse("Hello",safe=False)
